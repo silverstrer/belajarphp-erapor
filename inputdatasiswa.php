97098b3e69af261171834f257b5fe5c835148d4f
silverstrer <fathurrohman gala sanca>
2021-07-15 22:03:30 +0700

    Membuat form input data siswa dan terhubung ke tabel nilai

--- inputdatasiswa.php
+++ inputdatasiswa.php
@@ -4,13 +4,62 @@
 </head>
  
 <body>
+    <h2>Input Data Siswa</h2>
 	<form name="inputdatasiswa" method="post" action="inputdatasiswa.php">
-        <label for="nis_siswa">NIS :</label>
-		<input  style="height:30px;" size="2" name="nis_siswa[]" type="text" id="nis_siswa">
+        <table>
+            <tr>
+                <td><label for="nis_siswa">NIS</label></td>
+                <td><input  style="height:30px;" size="10" name="nis_siswa" type="text" id="nis_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="nisn_siswa">NISN</label></td>
+                <td><input  style="height:30px;" size="10" name="nisn_siswa" type="text" id="nisn_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="nama_siswa">Nama Siswa</label></td>
+                <td><input required style="height:30px;" size="50" name="nama_siswa" type="text" id="nama_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="kelas">Kelas</label></td>
+                <td><select style="height:30px;" name="kelas" id="kelas">
+                    <option value="I (satu)">I (satu)</option>
+                    <option value="II (dua )">II (dua)</option>
+                    <option value="III (tiga)">III (tiga)</option>
+                    <option value="IV (empat)">IV (empat)</option>
+                    <option value="V (lima)">V (lima)</option>
+                    <option value="VI (enam)">VI (enam)</option>
+                </select></td>
+            </tr>
+            <tr>
+                <td><label for="tempatlahir_siswa">Tempat lahir</label></td>
+                <td><input  style="height:30px;" size="20" name="tempatlahir_siswa" type="text" id="tempatlahir_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="tgllahir_siswa">Tanggal lahir</label></td>
+                <td><input  style="height:30px;" size="10" name="tgllahir_siswa" type="date" id="tgllahir_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="kelamin_siswa">Jenis Kelamin </label></td>
+                <td><select style="height:30px;" name="kelamin_siswa" id="kelamin_siswa">
+                    <option value="L">Laki-laki</option>
+                    <option value="P">Perempuan</option>
+                </select></td>
+            </tr>
+            <tr>
+                <td><label for="agama_siswa">Agama</label></td>
+                <td><input  style="height:30px;" size="10" name="agama_siswa" type="text" id="agama_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="pendseb_siswa">Pendidikan Sebelumnya</label></td>
+                <td><input  style="height:30px;" size="30" name="pendseb_siswa" type="text" id="pendseb_siswa"></td>
+            </tr>
+            <tr>
+                <td><label for="alamat_siswa">Alamat Siswa</label></td>
+                <td><input  style="height:30px;" size="50" name="alamat_siswa" type="text" id="alamat_siswa"></td>
+            </tr>
+        </table>
         <br>
-        <br>
-		<input type="submit" name="inputdatasiswa" value="Submit"></td>
-
+		<input type="submit" name="inputdatasiswa" value="Simpan"></td>
 	</form>
 </body>
 </html>
@@ -19,17 +68,30 @@
 /* Check if button name "Submit" is active, do this */
 if(isset($_POST['inputdatasiswa']))
 {
-	$nis_siswa=$_POST['nis_siswa'];
+	$nis_siswa = $_POST['nis_siswa'];
+    $nisn_siswa = $_POST['nisn_siswa'];
+    $nama_siswa = $_POST['nama_siswa'];
+    $kelas = $_POST['kelas'];
+    $tempatlahir_siswa = $_POST['tempatlahir_siswa'];
+    $tgllahir_siswa = $_POST['tgllahir_siswa'];
+    $kelamin_siswa = $_POST['kelamin_siswa'];
+    $agama_siswa = $_POST['agama_siswa'];
+    $pendseb_siswa = $_POST['pendseb_siswa'];
+    $alamat_siswa=$_POST['alamat_siswa'];
 
     include_once("config.php");
 
-    $result = mysqli_query($mysqli,"INSERT INTO datasiswa(nis_siswa) VALUES ('$nis_siswa')");
+    $result = mysqli_query($mysqli,"INSERT INTO datasiswa(no_siswa, nis_siswa, nisn_siswa, nama_siswa, kelas, tempatlahir_siswa, tgllahir_siswa, kelamin_siswa, agama_siswa, pendseb_siswa, alamat_siswa) VALUES ('','$nis_siswa','$nisn_siswa','$nama_siswa','$kelas','$tempatlahir_siswa','$tgllahir_siswa','$kelamin_siswa','$agama_siswa','$pendseb_siswa','$alamat_siswa')");
 
-    header("Location: masterinput.php");
-    mysqli_close($mysqli);
+    $nilai = mysqli_query($mysqli,"INSERT INTO nilaiptspkn(no_ptspkn, nilai_ptspkn) VALUES ('','0')");
+
+header("location:inputdatasiswa.php");
+mysqli_close($mysqli);
 
 }
 
 // source : https://t4tutorials.com/update-and-delete-multiple-records-together-in-php-mysql/
 // https://www.studentstutorial.com/php/php-update-multiple-row
 ?>
+<!-- Mereset AUTO INCREMENT -->
+<!-- ALTER TABLE tablename AUTO_INCREMENT = 1 -->
