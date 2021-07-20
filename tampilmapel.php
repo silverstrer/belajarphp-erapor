78c7f50bb14963f7b543b0d33debcb65e86db81c
silverstrer <fathurrohman gala sanca>
2021-07-20 20:02:14 +0700

    Membuat file tampilmapel.php

---
+++ tampilmapel.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <!-- KD Kelas 6 -->
+            <td>No.</td>
+            <td>Mata Pelajaran</td>
+            <td>Kompetensi Dasar</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Pendidikan Agama Islam</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a> |
+                <a href="">3.5</a> |
+                <a href="">3.6</a> |
+                <a href="">3.7</a> |
+                <a href="">3.8</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pendidikan Kewarganegaraan</td>
+            <td>
+                <a href="masterinputnilai.php">3.1</a> |
+                <a href="">3.2</a> |
+                <a href="`">3.3</a> |
+                <a href="">3.4</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Ilmu Pengetahuan Alam</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a> |
+                <a href="">3.5</a> |
+                <a href="">3.6</a> |
+                <a href="">3.7</a> 
+            </td>    
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Ilmu Pengetahuan Sosial</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Bahasa Indonesia</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a> |
+                <a href="">3.5</a> |
+                <a href="">3.6</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Matematika</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a> |
+                <a href="">3.5</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Pendidikan Jasmani, Olahraga dan Kesehatan</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a> |
+                <a href="">3.5</a> |
+                <a href="">3.6</a> |
+                <a href="">3.7</a>
+            </td>    
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Seni Budaya dan Prakarya</td>
+            <td>
+                <a href="">3.1</a> | 
+                <a href="">3.2</a> |
+                <a href="">3.3</a> |
+                <a href="">3.4</a>
+            </td>    
+        </tr>        
+    </table>
+</body>
+</html>
