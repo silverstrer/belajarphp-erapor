768797c01e8f53072f28d25a3080c5bd2abf8429
silverstrer <fathurrohman gala sanca>
2021-07-21 08:51:32 +0700

    perbaikan tampilan nilai : membuat menu tampilan nilai yang terhubung dengan masterinputnilaicopy.php

---
+++ tampilnilaicopy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Pendidikan Kewarganegaraan
+        <a href="masterinputnilaicopy.php?nilai=ptspkn31">ptspkn1</a>
+        <a href="masterinputnilaicopy.php?nilai=ptspkn32">ptspkn2</a>
+        <a href="masterinputnilaicopy.php?nilai=ptspkn33">ptspkn3</a>
+        <a href="masterinputnilaicopy.php?nilai=ptspkn34">ptspkn4</a>
+    </p>
+</body>
+</html>
